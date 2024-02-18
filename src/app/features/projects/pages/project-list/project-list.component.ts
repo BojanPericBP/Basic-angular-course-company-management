355b8140412c3fe936c7ef0d0efae3dbdbcb685c
b0jan <bojan@invenit.io>
2024-02-18 14:38:27 +0100

    feature: empty list holder, minor improvements

--- src/app/features/projects/pages/project-list/project-list.component.ts
+++ src/app/features/projects/pages/project-list/project-list.component.ts
@@ -5,11 +5,18 @@ import { ListWrapperComponent } from 'src/app/wrappers/list-wrapper/list-wrapper
 import { ProjectService } from '../../project.service';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
+import { EmptyListPlaceholderComponent } from 'src/app/core/components/empty-list-placeholder/empty-list-placeholder.component';
 
 @Component({
   selector: 'app-project-list',
   standalone: true,
-  imports: [CommonModule, ListWrapperComponent, MatListModule, MatIconModule],
+  imports: [
+    CommonModule,
+    ListWrapperComponent,
+    EmptyListPlaceholderComponent,
+    MatListModule,
+    MatIconModule,
+  ],
   templateUrl: './project-list.component.html',
   styleUrls: ['./project-list.component.scss'],
 })
