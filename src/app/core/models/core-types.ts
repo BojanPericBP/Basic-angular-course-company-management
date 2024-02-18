2369be0f6a232533a64cf15e80066bc6d224e696
b0jan <bojan@invenit.io>
2024-02-18 13:52:07 +0100

    feature: extend projects with property teams

--- src/app/core/models/core-types.ts
+++ src/app/core/models/core-types.ts
@@ -33,16 +33,17 @@ export interface ProjectInput {
   id: string | null;
   name: string;
   description: string;
+  teams: Team[];
 }
 
 export interface Project {
   id: string;
   name: string;
   description: string;
+  teams: Team[];
 }
 
 //TEAMS
-
 export interface TeamInput {
   id: string | null;
   name: string;
--- src/app/features/projects/pages/project-item/project-item.component.ts
+++ src/app/features/projects/pages/project-item/project-item.component.ts
@@ -5,6 +5,10 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { ProjectService } from '../../project.service';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { Team } from 'src/app/core/models/core-types';
+import { MatSelectModule } from '@angular/material/select';
+import { Observable } from 'rxjs';
+import { TeamsService } from 'src/app/features/teams/teams.service';
 
 @Component({
   selector: 'app-project-item',
@@ -15,15 +19,27 @@ import { MatInputModule } from '@angular/material/input';
     MatFormFieldModule,
     MatInputModule,
     ItemWrapperComponent,
+    MatSelectModule,
   ],
   templateUrl: './project-item.component.html',
   styleUrls: ['./project-item.component.scss'],
 })
 export class ProjectItemComponent {
   projectService = inject(ProjectService);
+  teamService = inject(TeamsService);
+
+  teams$: Observable<Team[]>;
+
   form = new FormGroup({
     id: new FormControl<string | null>(null),
     name: new FormControl<string | null>(null),
     description: new FormControl<string | null>(null),
+    teams: new FormControl<Team[] | null>(null),
   });
+
+  constructor() {
+    this.teams$ = this.teamService.getItems();
+  }
+
+  compareTeams = (o1: Team, o2: Team) => o1.id === o2.id;
 }
